23181e8580d7d59661eec24c4db9525e0637a347
Le Lin <lelin999@gmail.com>
2017-01-30 11:21:48 -0800

    created a program that tells you your birthday

---
+++ WebFoundamentals/Javascript/daysUntilBirthday.js
@@ -0,0 +1,17 @@
+var daysUntilBirthday = function(days) {
+	while (days > 0) {
+		if (days > 30) {
+			console.log(days " days until my birthday. Unlucky :(");
+		} else if (days > 5) {
+			console.log(days " until my birthday. Almost there!!!!");
+		} else {
+			console.log(days " DAYS UNTIL MY BIRTHDAY!!?!?!??!?!?");
+		}
+		days--;
+	}
+	if (days === 0) {
+		console.log("dank memes can't break playoff dreams.");
+	}
+};
+
+daysUntilBirthday(10);
