fbdb3bfab644995511cf4720219a804e75b28208
Le Lin <lelin999@gmail.com>
2017-02-02 09:23:20 -0800

    submitting now appends to table

--- WebFoundamentals/jQuery/Users/main.js
+++ WebFoundamentals/jQuery/Users/main.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
 	$('form').submit(function () {
+		var first_name_value = $("form input[name='firstname']").val();
+		var last_name_value = $("form input[name='lastname']").val();
+		var email_value = $("form input[name='email']").val();
+		var contact_value = $("form input[name='contact']").val();
+
+		$('.table1').append("<tr><td>" + "insert text here" + "</td><td>" + "other text" + "</td><td>" + "more text" + "</td><td>" + "last text" + "</td></tr>");
+		
 		return false;
-	})
-	$("form").submit(function (event) {
-		$('table').append('cat');
-	})
+	});
 });
-
-
-
-
-
-// $('form').submit(function() {
-// 	$('table').addClass('table');
-// });
