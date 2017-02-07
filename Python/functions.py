95c86d534db2fbf270d0d7f4717119f895e8c8a4
Le Lin <lelin999@gmail.com>
2017-02-06 19:19:31 -0800

    finished reqs

--- Python/functions.py
+++ Python/functions.py
@@ -27,3 +27,12 @@ def layered_multiples(arr):
 	return arr
 
 print layered_multiples(multiply([2,4,6,8], 3))
+
+# def layered_multiples(arr):
+# 	new_arr = []
+# 	for i in range(len(arr)):
+# 		array2 = []
+# 		for i in range(arr[i]):
+# 			array2.append(1)
+# 		new_arr.append(array2)
+# 	return new_arr
