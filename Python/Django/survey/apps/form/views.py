3d5ddabc5801c54941f7014552b69b357ddec1f8
Le Lin <lelin999@gmail.com>
2017-02-16 12:51:19 -0800

    survey has all fields except comments

---
+++ Python/Django/survey/apps/form/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.	
+def index(req):
+	if 'count' not in req.session:
+		req.session['count'] = 1
+	return render(req, 'form/index.html')
+
+def process(req):
+	if req.method == 'POST':
+		if req.POST['button'] == 'Go Back':
+			return render(req, 'form/index.html')
+		else:
+			req.session['count'] += 1
+			req.session['name'] = req.POST['name']
+			req.session['location'] = req.POST['location']
+			req.session['language'] = req.POST['language']
+			return redirect('/results')
+
+def results(req):
+	return render(req, 'form/results.html')
