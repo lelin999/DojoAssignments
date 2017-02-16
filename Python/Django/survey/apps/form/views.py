5ce8c5446f8196c3c79682005a87b5adf4d16ff0
Le Lin <lelin999@gmail.com>
2017-02-16 14:26:13 -0800

    used method that does not involve session

--- Python/Django/survey/apps/form/views.py
+++ Python/Django/survey/apps/form/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 
+context = {
+	'name': '',
+	'location': '',
+	'language': ''
+}
 # Create your views here.	
 def index(req):
 	if 'count' not in req.session:
@@ -11,11 +16,14 @@ def process(req):
 		if req.POST['button'] == 'Go Back':
 			return render(req, 'form/index.html')
 		else:
-			req.session['count'] += 1
-			req.session['name'] = req.POST['name']
-			req.session['location'] = req.POST['location']
-			req.session['language'] = req.POST['language']
+			# req.session['count'] += 1
+			# req.session['name'] = req.POST['name']
+			# req.session['location'] = req.POST['location']
+			# req.session['language'] = req.POST['language']
+			context['name'] = req.POST['name']
+			context['location'] = req.POST['location']
+			context['language'] = req.POST['language']
 			return redirect('/results')
 
 def results(req):
-	return render(req, 'form/results.html')
+	return render(req, 'form/results.html', context)
